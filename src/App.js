9bf5d0ce2c41e862357937c668669511d7181fe5
haluskua <haluskua.one@gmail.com>
2020-09-03 15:57:09 +1200

    added homepage component plus sassy stylesheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import HomePage from "./homepage.component";
 
 function App() {
   return (
     <div>
-      HELLOW WORLD
-     
+      <HomePage />
     </div>
   );
 }
